6cf49e59805f6e250952d546aa1de8de07010a4b
vbonhomme <bonhomme.vincent@gmail.com>
2024-08-22 16:14:30 +0200

    +balance

---
+++ R/helpers.R
@@ -0,0 +1,193 @@
+
+# permuting helpers  -----
+#' Balance a data.frame
+#'
+#' Balance with `n` individuals in each group, taken from the first column
+#'
+#' @param x data.frame with first column as grouping column
+#' @param n integer number of individuals in each group. Smallest sample size by default.
+#'
+#' @details Note that [sample]() is done with `replace=TRUE`
+#'
+#' @return balanced data.frame
+#'
+#' @examples
+#' # untouched dataset is not balanced
+#' table(pig$sp)
+#' # balance helps
+#' table(balance(pig)$sp)
+#' @export
+balance <- function(x, n=min(table(x[[1]]))) {
+  ids <- unlist(lapply(split(seq_along(x[[1]]), x[[1]]), sample, n, replace=TRUE))
+  x[ids, ]
+}
+#
+# # this one randomize labels in first column
+# randomize <- function(x){
+#   x[[1]] <- sample(x[[1]])
+#   x
+# }
+#
+# # this one retain n columns (all by default)
+# retain <- function(x, n=ncol(x)-1){
+#   x[, 1:(n+1)]
+# }
+#
+# # core functions -----
+# # a thin wrapper on MASS::lda that returns a (dimnamed) confusion matrix
+# lda1 <- function(x){
+#   actual <- x[[1]]
+#   pred <- MASS::lda(x[, -1], actual, CV=TRUE)$class
+#   table(actual, pred, dnn=c("actual", "predicted"))
+# }
+# iris %>% lda1
+#
+#
+# # confusion matrix helpers -----
+# acc <- function(x){
+#   (sum(diag(x))/sum(x)) %>% `names<-`("acc")
+# }
+#
+# acc_class <- function(x){
+#   diag(x)/rowSums(x)
+# }
+#
+# acc_all <- function(x){
+#   c(acc(x), acc_class(x))
+# }
+# x <- iris %>% lda1
+# x %>% acc()
+# x %>% acc_class()
+# x %>% acc_all()
+#
+# # cross-validation ----
+# #    - original: the dataset passed, untouched
+# #    - random:   original but shuffled on first (ie grouping) column
+# #    - balanced: original but balanced on first column
+# #    - balanced_random: balanced but randomized on first column
+#
+# partition_names <- c("original", "random", "balanced", "balanced_random")
+# partition <- function(x){
+#   list(original=x,
+#        random=x %>% randomize(),
+#        balanced=x %>% balance(),
+#        balanced_random=x %>% balance() %>% randomize()
+#   )
+# }
+#
+# # hooks ----
+# hook_bypass <- function(x) x
+# hook_pca <- function(x, ...) {
+#   scores <- stats::prcomp(x[, -1], ...)$x %>% tibble::as_tibble()
+#   bind_cols(dplyr::select(x, 1), scores)
+# }
+#
+#
+# iris %>% partition()
+# iris %>% partition() %>% map(hook_bypass)
+# iris %>% partition() %>% map(hook_pca)
+# iris %>% partition() %>%
+#   map(lda1) %>%
+#   map_dfr(~.x %>% acc_all) %>%
+#   dplyr::mutate(partition=partition_names, .before=1)
+#
+# cv1 <- function(x, retain){
+#   x %>%
+#     retain(retain) %>%
+#     partition() %>%
+#     purrr::map_dfr(~.x %>% lda1 %>% acc_all) %>%
+#     dplyr::mutate(retain=retain, partition=partition_names, .before=1)
+# }
+# cv12 <- function(x, retain){
+#   x %>%
+#     retain(retain) %>%
+#     partition() %>%
+#     map_dfr(~.x %>% lda1 %>% acc_all) -> x
+#   x$partition <- x$partition_names
+#   x
+# }
+#
+# microbenchmark::microbenchmark(cv1(pig2, 3), cv12(pig2, 3))
+#
+# cv1(pig2, 3)
+#
+#
+#
+#
+# cv2 <- function(x, max_retain=30){
+#   map_dfr(seq_len(max_retain), ~x %>% cv1(retain=.x))
+# }
+#
+# cv <- function(x, max_retain=30, iter=5){
+#   map_dfr(seq_len(iter), ~x %>% cv2(max_retain) %>% mutate(iter=.x, .before=1))
+# }
+#
+#
+# pig2 <- pig$mat %>% as_tibble() %>% mutate(sp=pig$gp, .before=1) %>% retain(30)
+#
+# pig2 %>% cv1(2)
+#
+# z <- map_dfr(seq_len(20), ~pig2 %>% cv2(30) %>% mutate(iter=.x, .before=1))
+#
+# partition_cols <- c("original"="#0392cf", balanced="#7bc043",
+#                     "balanced_random"="#f37736", "random"="#ee4035")
+# z %>%
+#   pivot_longer(acc:last_col(), names_to = "metric") %>%
+#   mutate(metric=ifelse(metric=="acc", "global", metric)) %>%
+#   mutate(partition=factor(partition, levels=names(partition_cols), ordered=TRUE)) %>%
+#   mutate(metric=factor(metric, c("global", "DP", "WB"), ordered = TRUE)) %>%
+#   ggplot() +
+#   aes(x=retain, y=value, col=partition) +
+#   geom_point(size=0.01, alpha=0.1) +
+#   # geom_density2d() +
+#   geom_smooth(linewidth=0.5) +
+#   scale_color_manual(values=partition_cols) +
+#   facet_grid(~metric) +
+#   ggtitle("mevolCVP rewrite") +
+#   xlab("nb of components") +
+#   ylab("accuracies") +
+#   theme_minimal() +
+#   scale_y_continuous(expand = c(0, 0)) +
+#   scale_x_continuous(expand = c(0, 0)) +
+#   theme(panel.grid.minor = element_blank(), legend.position = "bottom",
+#         panel.background = element_rect(colour="grey50"))
+# ggsave("mevolCVP2.pdf", width=9, height=5)
+#
+# z %>%
+#   pivot_longer(acc:last_col(), names_to = "metric") %>%
+#   mutate(metric=ifelse(metric=="acc", "global", metric)) %>%
+#   mutate(partition=factor(partition, levels=names(partition_cols), ordered=TRUE)) %>%
+#   mutate(metric=factor(metric, c("global", "DP", "WB"), ordered = TRUE)) %>%
+#   ggplot() +
+#   aes(x=retain, y=value, col=partition) +
+#   geom_point(size=0.01, alpha=0.1) +
+#   # geom_density2d() +
+#   geom_smooth(formula="y~x", method="loess", linewidth=0.5, level=0.95) +
+#   scale_color_manual(values=partition_cols) +
+#   facet_grid(~metric) +
+#   ggtitle("mevolCVP rewrite") +
+#   xlab("nb of components") +
+#   ylab("accuracies") +
+#   theme_minimal() +
+#   scale_y_continuous(expand = c(0, 0)) +
+#   scale_x_continuous(expand = c(0, 0)) +
+#   theme(panel.grid.minor = element_blank(), legend.position = "bottom",
+#         panel.background = element_rect(colour="grey50"))
+#
+#
+# table(g)
+#
+#
+#
+# expand_grid(iter=1:5, retain=1:10, partition=partition_names)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
