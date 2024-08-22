de07843330f93738223fb4e70aa2d8ef9aac3016
vbonhomme <bonhomme.vincent@gmail.com>
2024-08-22 16:00:15 +0200

    Initial commit

---
+++ R/pig.R
@@ -0,0 +1,5 @@
+#' Data from JAS paper
+#'
+#' @format a data.frame containing grouping factor as first column and the first 50 principal components
+#' @source  <https://doi.org/10.1016/j.jas.2012.08.005>
+"pig"
